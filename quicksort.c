378129f9e69245929168da2f8e5bf802148e9b57
DONGHAEY <80025157+DONGHAEY@users.noreply.github.com>
2021-12-08 22:18:37 +0900

    I modify printing and err, this code is quicksort
    I modify printing and err, this code is quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
-#define swap(x,y,t) ((t)=(y) (y)=(x) (x)=(t))
+#define swap(x,y,t) ((t)=(x), (x)=(y), (y)=(t))
 
-int quicksort(int list[], left,right) {
-    do {
-            int temp;
-    int low = left;
-    int high =right+1;
-    int pibot = list[0];
-    do {
-        low++;
-    }while (list[low] < pibot)
-    do{
-        high--;
-    }while(list[high]>pibot)
-
-    if(low<high) {
-        swap(list[low], list[high], temp);
-    }
-    }while (low<high)
-    swap(left, high, temp);
-    return high;
+int quick(int list[], int left, int right) {
+	int temp, low, high, pivot;
+	pivot = list[left];
+	low = left;
+	high = right+1;
+	
+	do {
+		do {
+			low++;
+		}while(list[low]<pivot);
+		
+		do {
+			high--;
+		}while(list[high]>pivot);
+		if(low<high) swap(list[low], list[high], temp);
+	} while(low<high);
+	swap(list[high], list[left], temp);
+	return high;
 }
 
-int sort(int list[], int left, int right) {
-    if (left<right) {
-        int mid = quicksort(list, left, right);
-        sort(list, left, mid-1);
-        sort(list, mid+1, right);
-    }
+void sort(int list[], int left, int right) {
+	if(left<right) {
+		int mid = quick(list, left, right);
+		sort(list, left, mid-1);
+		sort(list, mid+1, right);
+	}
 }
 
 int main() {
-    int arr[5] = {0,5,3,4,1};
-    sort(arr,0 ,4);
+	int arr[5] = {1, 5, 4, 2, 7};
+	sort(arr, 0, 4);
+	for(int i=0; i<5; i++) {
+		printf("%d ", arr[i]);
+	}
 }
